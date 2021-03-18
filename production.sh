535ec24e215c1129c75462053af64c1772249074
Caaalabash <1121062986@qq.com>
2021-03-18 10:57:03 +0800

    chore: sh

---
+++ production.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+project_name=test
+
+docker_compose_cmd="docker-compose -p ${project_name} -f deploy/docker-compose.yml"
+
+build(){
+    ${docker_compose_cmd} build
+}
+
+start(){
+    ${docker_compose_cmd} up -d
+}
+
+down(){
+    ${docker_compose_cmd} down
+}
+
+restart(){
+    ${docker_compose_cmd} down
+    sleep 2
+    ${docker_compose_cmd} up -d
+}
+
+if [ "$1" == "start" ]; then
+    start
+elif [ "$1" == "down" ]; then
+    down
+elif [ "$1" == "build" ]; then
+    build
+elif [ "$1" == "restart" ]; then
+    restart
+elif [ "$1" == "" ]; then
+    echo "help"
+fi
+
